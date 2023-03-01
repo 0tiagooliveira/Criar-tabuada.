83d2571840cf49d0d5598da89d273c512f30dfc3
0tiagooliveira <123430824+0tiagooliveira@users.noreply.github.com>
2023-03-01 16:49:46 -0300

    Create  Criar tabuada.py

---
+++ Criar tabuada.py
@@ -0,0 +1,22 @@
+x = int(input('Digite um número para criarmos uma tabuada:'))
+v1 = x * 1
+v2 = x * 2
+v3 = x * 3
+v4 = x * 4
+v5 = x * 5
+v6 = x * 6
+v7 = x * 7
+v8 = x * 8
+v9 = x * 9
+v10 = x * 10
+####################################
+print(x,'x 1 =',v1)
+print(x, 'x 2 =', v2)
+print(x, 'x 3 =', v3)
+print(x, 'x 4 =', v4)
+print(x, 'x 5 =', v5)
+print(x, 'x 6 =', v6)
+print(x, 'x 7 =', v7)
+print(x, 'x 8 =', v8)
+print(x, 'x 9 =', v9)
+print(x, 'x 10 =', v10)
